7793fe6cfd46097d7aa266aa42d8162dbc7f2141
FABIANA ESTELA DA SILVA YAMANA <44611897+estelayamana@users.noreply.github.com>
2022-06-08 16:44:10 -0300

    Add files via upload

---
+++ Exercicio-Pratico-2.js
@@ -0,0 +1,8 @@
+const distancia = 10000;
+console.log(`${distancia} m`);
+
+const intervaloTempo = 3600;
+console.log(`${intervaloTempo} s`);
+
+const velocidadeMedia= distancia / intervaloTempo;
+console.log(`A distância percorrida é: ${velocidadeMedia} m/s`); 
